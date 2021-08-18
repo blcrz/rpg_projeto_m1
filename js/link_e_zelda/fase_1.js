e3f2bb0c0059f0fe7dd6ca23172e1972923436f9
Bárbara Cruz <ba.luizacruz@gmail.com>
2021-08-18 11:31:04 -0300

    alterações de estilo

--- js/link_e_zelda/fase_1.js
+++ js/link_e_zelda/fase_1.js
@@ -4,17 +4,28 @@ var button = document.getElementById('play')
 button.focus();
 
 function iniciarFase(){
-    let result = prompt('Você têm duas escolhas:\n1. Entrar no castelo Hyrule\n2. Ir para a floresta em busca da Master Sword (uma espada com poderes mágicos)')
+    let textoUsuario = 'Você têm duas escolhas:\n1. Entrar no castelo Hyrule\n2. Ir para a floresta em busca da Master Sword (uma espada com poderes mágicos)'
+    let result = inserirDado(textoUsuario)
+
+    // while(result !== '1' && result !=='2'){
+    while(!['1','2'].includes(result) && result !== null){
+        alert('Insira uma opção válida')
+        result =  inserirDado(textoUsuario)
+    }
 
     if(result === '1'){
         texto.innerHTML = 'Um dos soldados te acertou'
         img.src = '../../img/link_e_zelda/fase_1/escolha_1.gif'
         button.onclick = seguirEmFrente
-    }else {
+    }else if(result === '2'){
         texto.innerHTML = 'Você encontrou a Master Sword'
         img.src = '../../img/link_e_zelda/fase_1/escolha_2.gif'
         button.onclick = entrarNoCastelo
     }
+}
+
+function inserirDado(textoUsuario){
+    return prompt(textoUsuario)
 }
 
 function seguirEmFrente(){
